285f5fced5b5b20d77ef4c1d7f8182082c3cd521
Andrey <blackwathc7658@mail.ru>
2025-12-21 03:29:03 +0300

    minor fixes in db API

--- lib/db.ts
+++ lib/db.ts
@@ -5,34 +5,48 @@ import path from "path"
 const db_path = path.join(process.cwd(), "lib/data", "database.db");
 console.log(db_path);
 
-const db = new Database(db_path);
+let db: Database.Database;
 
 
 namespace API {
 
     export function __getChapters() {
-        return db.prepare("SELECT * FROM chapters").all();
+        db = new Database(db_path);
+        const data = db.prepare("SELECT * FROM chapters").all();
+        db.close()
+        return data;
     }
 
     export function __getLessons() {
-        return db.prepare("SELECT * FROM lessons").all();
+        db = new Database(db_path);
+        const data= db.prepare("SELECT * FROM lessons").all();
+        db.close()
+        return data;
     }
 
 
     export function __getChapterById(id: number) {
+        db = new Database(db_path);
         const res = db.prepare("SELECT * FROM chapters WHERE id=?");
-        return res.run(id);
+        const data= res.run(id);
+        db.close()
+        return data;
     }
 
     export function __getLessonsByChapter(chapter: number) {
+        db = new Database(db_path);
         const res = db.prepare("SELECT * FROM lessons WHERE chapter=?")
         const res2 = res.run(chapter);
-        return db.prepare("SELECT * FROM lessons WHERE chapter=?").get(res2.lastInsertRowid);
+        const data= db.prepare("SELECT * FROM lessons WHERE chapter=?").get(res2.lastInsertRowid);
+        db.close()
+        return data;
     }
 
     export function __getLessonsById(id1: number) {
-        const res = db.prepare("SELECT * FROM lessons WHERE id=?").get(id1)
-        return res;
+        db = new Database(db_path);
+        const data = db.prepare("SELECT * FROM lessons WHERE id=?").get(id1)
+        db.close()
+        return data;
 
     }
 
